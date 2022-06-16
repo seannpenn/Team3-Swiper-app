09879f24319c92364d169e18e6fb10a9f9141579
Intel Padz <padigos1000@gmail.com>
2022-06-16 23:53:50 +0800

    authcreen&profile_smallchanges

--- lib/src/screens/profile/profile_screen.dart
+++ lib/src/screens/profile/profile_screen.dart
@@ -41,128 +41,144 @@ class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-<<<<<<< Updated upstream
-      
-      resizeToAvoidBottomInset: true,
-=======
       resizeToAvoidBottomInset: false,
->>>>>>> Stashed changes
-      body: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topRight,
-            end: Alignment.bottomLeft,
-            colors: [
-              Color.fromARGB(255, 56, 208, 193),
-              Color.fromARGB(194, 1, 61, 85)
-            ],
-          ),
-        ),
-        child: SizedBox(
-          height: MediaQuery.of(context).size.height,
-          width: MediaQuery.of(context).size.width,
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              children: [
-                InkWell(
-                  onTap: () {
-                    ImageService.updateProfileImage();
-                  },
-                  child: AvatarImage(
-                    uid: FirebaseAuth.instance.currentUser!.uid,
-                    radius: 80,
-                  ),
-                ),
-                const Padding(
-                  padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
-                  child: SizedBox(
-                    width: 300,
-                    height: 50,
-                    child: TextField(
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        hintText: 'Add bio',
-                      ),
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Container(
-                    width: 500,
-                    height: 50,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(40.0),
-                      color: Color.fromARGB(255, 158, 188, 188),
-                    ),
-                    child: const Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Center(
-                          child: Text(
-                            'Name',
-                            style: TextStyle(
-                              color: Color.fromARGB(255, 0, 0, 0),
-                            ),
-                          ),
-                        )),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Container(
-                    width: 500,
-                    height: 50,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(40.0),
-                      color: Color.fromARGB(255, 158, 188, 188),
-                    ),
-                    child: Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Center(
-                          child: Text(
-                            '@${user?.username}',
-                            style: const TextStyle(
-                              fontSize: 15,
-                            ),
-                          ),
-                        )),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Container(
-                    width: 500,
-                    height: 50,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(40.0),
-                      color: Color.fromARGB(255, 158, 188, 188),
-                    ),
-                    child: Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Center(
-                          child: Text(
-                            '${user?.email}',
-                            style: const TextStyle(
-                              color: Color.fromARGB(255, 0, 0, 0),
-                            ),
-                          ),
-                        )),
-                  ),
-                ),
-                ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    shadowColor: Color.fromARGB(255, 0, 0, 0),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30.0),
-                    ),
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 30, vertical: 10),
-                  ),
-                  child: const Text('Sign Out'),
-                ),
+      body: SingleChildScrollView(
+        child: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+              colors: [
+                Color.fromARGB(255, 56, 208, 193),
+                Color.fromARGB(194, 1, 61, 85)
               ],
+            ),
+          ),
+          child: SizedBox(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: [
+                  InkWell(
+                    onTap: () {
+                      ImageService.updateProfileImage();
+                    },
+                    child: AvatarImage(
+                      uid: FirebaseAuth.instance.currentUser!.uid,
+                      radius: 80,
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(top: 20, bottom: 20),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: TextFormField(
+                            onFieldSubmitted: (String text) {
+                              // send();
+                            },
+                            // focusNode: _messageFN,
+                            // controller: _messageController,
+                            decoration: InputDecoration(
+                              hintText: "Add Bio...",
+                              suffixIcon: IconButton(
+                                icon: const Icon(
+                                  Icons.done,
+                                  color: Color.fromARGB(255, 241, 241, 241),
+                                ),
+                                onPressed: () {},
+                              ),
+                              fillColor: Colors.white,
+                              border: const OutlineInputBorder(
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(8),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      width: 500,
+                      height: 50,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(40.0),
+                        color: const Color.fromARGB(255, 158, 188, 188),
+                      ),
+                      child: const Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Center(
+                            child: Text(
+                              'Name',
+                              style: TextStyle(
+                                color: Color.fromARGB(255, 0, 0, 0),
+                              ),
+                            ),
+                          )),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      width: 500,
+                      height: 50,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(40.0),
+                        color: Color.fromARGB(255, 158, 188, 188),
+                      ),
+                      child: Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Center(
+                            child: Text(
+                              '@${user?.username}',
+                              style: const TextStyle(
+                                fontSize: 15,
+                              ),
+                            ),
+                          )),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      width: 500,
+                      height: 50,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(40.0),
+                        color: Color.fromARGB(255, 158, 188, 188),
+                      ),
+                      child: Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Center(
+                            child: Text(
+                              '${user?.email}',
+                              style: const TextStyle(
+                                color: Color.fromARGB(255, 0, 0, 0),
+                              ),
+                            ),
+                          )),
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      shadowColor: Color.fromARGB(255, 0, 0, 0),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30.0),
+                      ),
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 30, vertical: 10),
+                    ),
+                    child: const Text('Sign Out'),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
